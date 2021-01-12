e92d5097bf6696a60e547a563036ffa628712566
defidel <remit-project@protonmail.com>
2021-01-12 15:10:28 -0800

    CI@1605830425

---
+++ precache-manifest.4f14808591a6ff3573494502efaa1a4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "335cb540ceaa5d4c81d2",
+    "url": "/css/app.6f7f9671.css"
+  },
+  {
+    "revision": "f6ce9449f99c6bbdeddfd1d77c65df4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "335cb540ceaa5d4c81d2",
+    "url": "/js/app.46575b58.js"
+  },
+  {
+    "revision": "857463508a59a2377fa2",
+    "url": "/js/chunk-vendors.4a9b8d50.js"
+  },
+  {
+    "revision": "4b14c64efaf846819b9a229b4193c8b7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
